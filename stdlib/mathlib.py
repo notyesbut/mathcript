e45540762e2aa1914445872b5a403377e4e637ce
notyesbut <139022100+notyesbut@users.noreply.github.com>
2024-10-02 16:50:54 -0400

    Create mathlib.py

---
+++ stdlib/mathlib.py
@@ -0,0 +1,24 @@
+# mathscript/stdlib/mathlib.py
+
+import math
+
+def sin(x):
+    return math.sin(x)
+
+def cos(x):
+    return math.cos(x)
+
+def tan(x):
+    return math.tan(x)
+
+def log(x, base=math.e):
+    return math.log(x, base)
+
+def exp(x):
+    return math.exp(x)
+
+def sqrt(x):
+    return math.sqrt(x)
+
+def pow(x, y):
+    return math.pow(x, y)
